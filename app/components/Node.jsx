7abd91b7a1e5a83ce86cef717a5423c30b220bf2
graham-hughes-code <grahamhughes@optonline.net>
2023-09-18 13:47:30 -0400

    import-wp-fix
    - fixed the import of example web componet

--- app/components/Node.jsx
+++ app/components/Node.jsx
@@ -1,5 +1,5 @@
 "use client"
-import { useCallback, useMemo } from 'react';
+import { useCallback, useMemo, useEffect } from 'react';
 import { useUpdateNodeInternals } from 'reactflow';
 import { Handle, Position } from 'reactflow';
 import { useNodeId } from 'reactflow';
@@ -7,9 +7,6 @@ import { wrapWc } from 'wc-react';
 
 import './Node.css';
 
-// import('./testWebCom');
-
-// const HelloWorld = wrapWc('hello-world');
 
 function Node({data, isConnectable}) {
   const updateNodeInternals = useUpdateNodeInternals();
@@ -20,6 +17,9 @@ function Node({data, isConnectable}) {
   // import("data:text/javascript,export function doSomething(str) { return str + '-done'; }").then((moduleData) => {
   //   console.log(moduleData.doSomething('test'));
   // })
+
+  useEffect(() => {import("../components/testWebCom.js"); console.log('afjadjf')}, []);
+  const HelloWorld = wrapWc('hello-world');
 
   const inlets =  def.inlets.map((e, i) => {
     return <Handle key={e.id} id={e.id} type="target" position={Position.Left} style={{ top: 25 + i * 10}} isConnectable={isConnectable} className='custom-node-handle'/>;
@@ -38,7 +38,7 @@ function Node({data, isConnectable}) {
       <div>
         <div className='custom-node-tittle'>{data.def.name.toUpperCase()}</div>
         <div className='nodrag' style={{margin: 6, fontSize: ".5rem", color: 'black'}}>
-          {/* <HelloWorld data={''} data_callback={handleChange}></HelloWorld> */}
+          <HelloWorld data={''} data_callback={handleChange}></HelloWorld>
         </div>
       </div>
     </div>
