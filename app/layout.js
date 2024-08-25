5302f10b5ab7c3d39cbf7c21396a8ec67102ce25
graham-hughes-code <grahamhughes@optonline.net>
2024-08-25 13:47:39 -0400

    change name to flowato

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "flowato",
-  description: "flowato-prototype",
+  description: "A flow based programming language.",
 };
 
 export default function RootLayout({ children }) {
